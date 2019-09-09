f889341c471c25231f95365b14bb55eec0e88f97
Ales Justin <ales.justin@gmail.com>
2019-09-09 17:55:40 +0200

    Add REST client.

---
+++ client/src/main/java/io/apicurio/registry/client/RegistryInterceptor.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 Red Hat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.apicurio.registry.client;
+
+import java.io.IOException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.ext.ReaderInterceptor;
+import javax.ws.rs.ext.ReaderInterceptorContext;
+import javax.ws.rs.ext.WriterInterceptor;
+import javax.ws.rs.ext.WriterInterceptorContext;
+
+/**
+ * @author Ales Justin
+ */
+public class RegistryInterceptor implements ReaderInterceptor, WriterInterceptor {
+
+    // TODO -- cache
+
+    @Override
+    public Object aroundReadFrom(ReaderInterceptorContext context) throws IOException, WebApplicationException {
+        System.out.println("context = " + context);
+        return context.proceed();
+    }
+
+    @Override
+    public void aroundWriteTo(WriterInterceptorContext context) throws IOException, WebApplicationException {
+        System.out.println("context = " + context);
+        context.proceed();
+    }
+}
